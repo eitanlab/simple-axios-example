c2c1c7640bed857cf55731732bc9de6a28e2f330
Eitan Lab <46536280+eitanlab@users.noreply.github.com>
2022-01-03 18:16:02 -0300

    Added loading state

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import "./styles.css";
-import axios from 'axios';
+import axios from "axios";
 
 export default function App() {
-  const [data, setData] = useState(null)
-  
-  useEffect( () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get('https://api2.binance.com/api/v3/ticker/24hr')
+      setLoading(true);
+      const response = await axios.get(
+        "https://api2.binance.com/api/v3/ticker/24hr"
+      );
       // console.log(response.data);
       setData(response.data);
-    }
-    fetchData(); 
-  },[]);
-  
-  const parsedData = data => {
-      return data.map(item => {
-        return (
-          <tr>
-            <td>{item.symbol}</td>
-            <td>{item.lastPrice}</td>
-            <td>{item.priceChange}</td>
-          </tr>
-        )
-      })
-  }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const parsedData = (data) => {
+    return data.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td>{item.symbol}</td>
+          <td>{item.lastPrice}</td>
+          <td>{item.priceChange}</td>
+        </tr>
+      );
+    });
+  };
 
   return (
     <div className="App">
       <h1>Binance cryptos</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <table>
-        <thead style={{backgroundColor: 'black', color: 'white'}}>
-          <tr>
-            <td>Symbol</td>
-            <td>Last Price</td>
-            <td>Price Change</td>
-          </tr>
-        </thead>
-        <tbody>
-          {data && parsedData(data)}
-        </tbody>
-      </table>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <table>
+          <thead style={{ backgroundColor: "black", color: "white" }}>
+            <tr>
+              <td>Symbol</td>
+              <td>Last Price</td>
+              <td>Price Change</td>
+            </tr>
+          </thead>
+          <tbody>{data && parsedData(data)}</tbody>
+        </table>
+      )}
     </div>
   );
 }
